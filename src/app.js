60ec8721477bf22e778aaa3ca818fa11d2354dce
Milena Martins <milenammagalhaes@gmail.com>
2022-01-27 18:00:23 -0500

    Added Prettier, ESlint, gitIgnore and Json to the project

--- src/app.js
+++ src/app.js
@@ -1,30 +1,32 @@
+/* global React ReactDOM */
+
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h2", {}, props.name),
     React.createElement("h3", {}, props.animal),
     React.createElement("h3", {}, props.breed),
-  ])
-}
+  ]);
+};
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: "Luna", 
-      animal: "Dog", 
+      name: "Luna",
+      animal: "Dog",
       breed: "Havanese"
     }),
     React.createElement(Pet, {
-      name: "Pepper", 
-      animal: "Bird", 
+      name: "Pepper",
+      animal: "Bird",
       breed: "Cockatiel"
     }),
     React.createElement(Pet, {
-      name: "Doink", 
-      animal: "Cat", 
+      name: "Doink",
+      animal: "Cat",
       breed: "Mix"
     }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
