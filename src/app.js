877de8a1ee3fdbd406d010b7d805e01513967727
Milena Martins <milenammagalhaes@gmail.com>
2022-01-28 11:32:29 -0500

    Created SearchParam

--- src/app.js
+++ src/app.js
@@ -1,33 +1,12 @@
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese"
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel"
-//     }),
-//     React.createElement(Pet, {
-//       name: "Doink",
-//       animal: "Cat",
-//       breed: "Mix"
-//     }),
-//   ]);
-// };
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
