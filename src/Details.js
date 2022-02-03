5ab10b487ab885026c028deffe44d56e71f0eb11
Milena Martins <milenammagalhaes@gmail.com>
2022-02-03 12:22:40 -0500

    Created page Details

---
+++ src/Details.js
@@ -0,0 +1,43 @@
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = { loading: true };
+  }
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+  render() {
+    const { name, animal, breed, city, state, description } = this.state;
+    return (
+      <div>
+        <div className="details">
+          <div></div>
+
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name} </button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
